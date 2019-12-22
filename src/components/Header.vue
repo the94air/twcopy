e313d152010fe44cde071d8e173096314852b94e
Abdalla Arbab <the94air@users.noreply.github.com>
2019-12-22 22:06:18 +0200

    Adding more info in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,8 @@
     <div class="container mx-auto px-4">
       <div class="text-center py-24">
         <h1 class="text-3xl md:text-4xl lg:text-5xl mb-4">Tailwind Copy 🌪️</h1>
-        <p class="text-gray-800 text-lg mb-6 mx-5">Easily copy Tailwindcss colors and shadows</p>
+        <p class="text-gray-800 text-lg mb-3 mx-5">Easily copy Tailwindcss colors and shadows</p>
+        <p class="text-gray-700 text-sm mb-8 mx-5">Useful when you're designing or when you're not using Tailwind for front-end</p>
         <a class="uppercase font-bold font-serif text-gray-800 no-underline rounded px-5 py-2" target="_blank" href="https://github.com/the94air/twcopy">Github</a>
       </div>
     </div>
